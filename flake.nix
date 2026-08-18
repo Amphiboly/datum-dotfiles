ba8323962712c397aa06d9012944d2844c724f2c
Rik Kabel <rik@panix.com>
2026-08-18 13:16:16 -0400

    fix: remove broken nixos-cosmic flake input and use native nixpkgs modules

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       url = "github:nix-community/lanzaboote/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #   gaze.url = "github:GunduLabs/gaze";
+    gaze.url = "github:GunduLabs/gaze";
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -37,7 +37,7 @@
     home-manager,
     sops-nix,
     lanzaboote,
-    #   gaze,
+    gaze,
     nur,
     ...
   } @ inputs: let
