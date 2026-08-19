b0bb60e7e725da4c41f8d69adeca3d0dddabf63c
Rik Kabel <rik@panix.com>
2026-08-19 17:18:29 -0400

    feature: enabling gaze

--- configuration.nix
+++ configuration.nix
@@ -297,7 +297,7 @@
 
   environment.etc."gaze/config.toml".text = ''
     [cameras]
-    rgb = "v4l2src device=/dev/video0 ! video/x-raw,format=YUYV,width=160,height=120 ! videoconvert ! appsink"
+    rgb = "v4l2src device=/dev/video0 ! video/x-raw,format=YUYV,width=160,height=120"
     ir = ""
     emitter_enabled = false
     dark_luma_threshold = 20
